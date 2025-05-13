c0c1e24ebad17ca8ed1bcd42db564dde9ef98407
Gabrielmanuel11 <gabrielmanuel11@outlook.com>
2025-05-13 10:32:35 -0300

    Relation 1-n created

--- SQL-in-NodeJS-and-Prisma-ORM/04-introducing-to-prisma/index.js
+++ SQL-in-NodeJS-and-Prisma-ORM/04-introducing-to-prisma/index.js
@@ -3,22 +3,26 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.post.createMany({
-    data: [
-      {
-        title: "Olá, mundo!",
-        content: "Este é o meu primeiro post criado com a ajuda do Prisma ORM!",
-        published: true
-      },
-      {
-        title: "Post 2",
-        content: null
+  await prisma.user.create({
+    data: {
+      name: "Gabriel",
+      email: "gabriel@email.com",
+      posts: {
+        create: [
+          {
+            title: "Post 1",
+            content: "Conteúdo do post 1",
+          }
+        ]
       }
-    ]
+    }
   })
+
+  const result = await prisma.user.findMany({
+    include: { posts: true }
+  })
+
+  console.log(result, result[0].posts)
 }
 
-main().then(async () => {
-  const result = await prisma.post.findMany()
-  console.table(result)
-})
+main()
